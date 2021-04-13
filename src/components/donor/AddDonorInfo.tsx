76e014a30474f1eb457da9e810017ad7ac67665b
nedhro <nedhro786>
2021-04-13 17:20:12 +0600

    Suitability Test List added

---
+++ src/components/donor/AddDonorInfo.tsx
@@ -0,0 +1,332 @@
+import React from "react";
+import DonorService from "../../services/DonorService";
+import { withRouter } from "react-router-dom";
+
+class AddDonorInfo extends React.Component<any, any> {
+  dataConfig: any = {};
+
+  changeHandler = (event: any) => {
+    this.setState({ [event.target.name]: event.target.value });
+    console.log(this.state);
+    if (event.target.name === "concernName") {
+      const concernSet = {
+        concernName: this.state.concernName,
+        concernStatus: "Yes",
+      };
+      this.dataConfig.concernSet.push(concernSet);
+    }
+  };
+
+  submitHandler = (event: any) => {
+    event.preventDefault();
+    this.dataConfig = {
+      donorName: this.state.donorName,
+      donorAge: this.state.donorAge,
+      donorGuardian: this.state.donorGuardian,
+      donorGender: this.state.donorGender,
+      donorMaritalStatus: this.state.donorMaritalStatus,
+      donorProfession: this.state.donorProfession,
+      donorPresentAddress: this.state.donorPresentAddress,
+      donorPermanentAddress: this.state.donorPermanentAddress,
+      donorMobileNo: this.state.donorMobileNo,
+      donorLastDonatedDate: this.state.donorLastDonatedDate,
+      donorLastDonatedPlace: this.state.donorLastDonatedPlace,
+      concernSet: [],
+    };
+    console.log(this.dataConfig);
+    this.submitDonorInfo(this.dataConfig);
+  };
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      donorName: "",
+      donorAge: "",
+      donorGuardian: "",
+      donorGender: "",
+      donorMaritalStatus: "",
+      donorProfession: "",
+      donorPresentAddress: "",
+      donorPermanentAddress: "",
+      donorMobileNo: "",
+      donorLastDonatedDate: "",
+      donorLastDonatedPlace: "",
+      concernName: "",
+      concernStatus: "",
+      notification: "",
+    };
+    this.submitHandler = this.submitHandler.bind(this);
+    this.changeHandler = this.changeHandler.bind(this);
+    this.submitDonorInfo = this.submitDonorInfo.bind(this);
+  }
+
+  componentDidMount() {}
+
+  submitDonorInfo(dataConfig: any) {
+    DonorService.saveDonorInfo(dataConfig).then((res) => {
+      console.log(res);
+      if (res.status === 201) {
+        this.setState({ notification: "Donor Info Added Successfully" });
+        this.props.history.push("/questionnaire/list");
+      }
+      this.setState({
+        notification: "Please add valid and non duplicate values",
+      });
+    });
+  }
+
+  render() {
+    const { notification } = this.state;
+    return (
+      <div className="container-fluid m-1 p-1">
+        <h2 className="text-info text-center">Add Donor Information</h2>
+        <div className="container p-1">
+          <form className="form" onSubmit={this.submitHandler}>
+            <div className="row">
+              <div className="col-6">
+                <div className="row form-group">
+                  <div className="col-4 text-right">
+                    <label htmlFor="donorName">Full Name</label>
+                  </div>
+                  <div className="col-8">
+                    <input
+                      className="form-control"
+                      type="text"
+                      name="donorName"
+                      id="donorName"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="col-6">
+                <div className="row form-group">
+                  <div className="col-4 text-right">
+                    <label htmlFor="donorAge">Age</label>
+                  </div>
+                  <div className="col-8">
+                    <input
+                      type="number"
+                      className="form-control"
+                      name="donorAge"
+                      id="donorAge"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-6">
+                <div className="row form-group">
+                  <div className="col-4 text-right">
+                    <label htmlFor="donorGuardian">Guardian/Relative</label>
+                  </div>
+                  <div className="col-8">
+                    <input
+                      className="form-control"
+                      type="text"
+                      name="donorGuardian"
+                      id="donorGuardian"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="col-6">
+                <div className="row form-group">
+                  <div className="col-4 text-right">
+                    <label htmlFor="donorProfession">Profession</label>
+                  </div>
+                  <div className="col-8">
+                    <input
+                      className="form-control"
+                      type="text"
+                      name="donorProfession"
+                      id="donorProfession"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-6">
+                <div className="row form-group">
+                  <div className="col-4 text-right">
+                    <label htmlFor="donorGender">Gender</label>
+                  </div>
+                  <div className="col-8">
+                    <select
+                      className="form-control"
+                      name="donorGender"
+                      id="donorGender"
+                      onChange={this.changeHandler}
+                    >
+                      <option value="">Select</option>
+                      <option value="Male">Male</option>
+                      <option value="Female">Female</option>
+                      <option value="Other">Other</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div className="col-6">
+                <div className="row form-group">
+                  <div className="col-4 text-right">
+                    <label htmlFor="donorMaritalStatus">Marital Status</label>
+                  </div>
+                  <div className="col-8">
+                    <select
+                      className="form-control"
+                      name="donorMaritalStatus"
+                      id="donorMaritalStatus"
+                      onChange={this.changeHandler}
+                    >
+                      <option value="">Select</option>
+                      <option value="Single">Single</option>
+                      <option value="Married">Married</option>
+                      <option value="Widowed">Widowed</option>
+                      <option value="Separated">Separated</option>
+                      <option value="Divorced">Divorced</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-6">
+                <div className="row form-group">
+                  <div className="col-4 text-right">
+                    <label htmlFor="donorMobileNo">Mobile</label>
+                  </div>
+                  <div className="col-8">
+                    <input
+                      className="form-control"
+                      type="text"
+                      name="donorMobileNo"
+                      id="donorMobileNo"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="col-6"></div>
+            </div>
+
+            <div className="row">
+              <div className="col-6">
+                <div className="row form-group">
+                  <div className="col-4 text-right">
+                    <label htmlFor="donorPresentAddress">Present Address</label>
+                  </div>
+                  <div className="col-8">
+                    <input
+                      className="form-control"
+                      type="text"
+                      name="donorPresentAddress"
+                      id="donorPresentAddress"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="col-6">
+                <div className="row form-group">
+                  <div className="col-4 text-right">
+                    <label htmlFor="donorPermanentAddress">
+                      Permanent Address
+                    </label>
+                  </div>
+                  <div className="col-8">
+                    <input
+                      className="form-control"
+                      type="text"
+                      name="donorPermanentAddress"
+                      id="donorPermanentAddress"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-6">
+                <div className="row form-group">
+                  <div className="col-4 text-right">
+                    <label htmlFor="donorLastDonatedDate">
+                      Last Donated Date
+                    </label>
+                  </div>
+                  <div className="col-8">
+                    <input
+                      className="form-control"
+                      type="date"
+                      pattern="yyyy-MM-dd"
+                      name="donorLastDonatedDate"
+                      id="donorLastDonatedDate"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="col-6">
+                <div className="row form-group">
+                  <div className="col-4 text-right">
+                    <label htmlFor="donorLastDonatedPlace">
+                      Last Donated Place
+                    </label>
+                  </div>
+                  <div className="col-8">
+                    <input
+                      className="form-control"
+                      type="text"
+                      name="donorLastDonatedPlace"
+                      id="donorLastDonatedPlace"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+                <div className="col-6">
+
+                </div>
+            </div>
+
+            <div className="row form-group">
+                <div className="col-2"></div>
+              <div className="col-4 m-1 p-1 float-right text-right">
+              <input
+                  type="submit"
+                  className="form-control btn btn-success m-1 p-1"
+                  value="Save"
+                />
+              </div>
+              <div className="col-4 m-1 p-1 float-right text-right">
+                <input
+                  type="reset"
+                  className="form-control btn btn-danger m-1 p-1"
+                  value="Reset"
+                />
+              </div>
+            </div>
+          </form>
+          <div className="text-danger">
+            <p className="text-center bg-info font-weight-bold">
+              {notification}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default withRouter(AddDonorInfo);
--- src/components/donor/PhysicalSuitability.tsx
+++ src/components/donor/PhysicalSuitability.tsx
@@ -1,16 +1,163 @@
 import React from "react";
+import DataTable from "react-data-table-component";
+import DonorService from "../../services/DonorService";
 
-class PhysicalSuitability extends React.Component<any,any>{
+class PhysicalSuitability extends React.Component<any, any> {
+  columns: any = [
+    {
+      name: "Hemoglobin",
+      selector: "donorHemoglobin",
+      sortable: true,
+    },
+    {
+      name: "Weight",
+      selector: "donorWeight",
+      sortable: true,
+    },
+    {
+      name: "Blood Pressure",
+      selector: "donorBloodPressure",
+      sortable: true,
+    },
+    {
+      name: "Pulse Rate",
+      selector: "donorPulseRate",
+      sortable: true,
+    },
+    {
+      name: "Temperature",
+      selector: "donorTemperature",
+      sortable: true,
+    },
+    {
+      name: "Blood Group",
+      selector: "donorBloodGroup",
+      sortable: true,
+    },
+    {
+      name: "Blood Group Rhesus",
+      selector: "donorBloodGroupRhesus",
+      sortable: true,
+    },
+    {
+      name: "Permission",
+      selector: "donorSelection",
+      sortable: true,
+    },
+    {
+      name: "Action",
+      selector: "action",
+      sortable: false,
+    },
+  ];
 
-    componentDidMount(){ }
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      error: null,
+      items: [],
+      notification: "",
+    };
+  }
 
-    render(){
-        return(
-            <div>
-                <h2>Donor Physical Suitability Test</h2>
+  componentDidMount() {
+    this.loadTests();
+  }
+
+  loadTests() {
+    let dataFinal: any = [];
+    DonorService.getPhysicalSuitabilityResults()
+      .then((res) => {
+        console.log(res);
+        let keys = [
+          "donorHemoglobin",
+          "donorWeight",
+          "donorBloodPressure",
+          "donorPulseRate",
+          "donorTemperature",
+          "donorBloodGroup",
+          "donorBloodGroupRhesus",
+          "donorSelection",
+          "action",
+        ];
+        let entries = this.filterData(res.data, keys);
+        //rows
+        entries.map((entry: any) => dataFinal.push(entry));
+        this.setState({
+          isLoaded: true,
+          items: dataFinal,
+        });
+      })
+      .catch((err) => console.log(err));
+    this.setState({ isLoaded: false });
+  }
+
+  filterData(dataArr: any, keys: any) {
+    let data = dataArr.map((entry: any) => {
+      let filteredEntry: any = {};
+      keys.forEach((key: any) => {
+        if (key in entry) {
+          filteredEntry[key] = entry[key];
+        }
+        if (key === "action") {
+          filteredEntry[key] =
+            '<button type="button" class="btn btn-indigo btn-sm m-0 " @click="editUserModal(item)"  >Edit</button> <button type="button" class="btn btn-red btn-sm m-0 ">Delete</button> ';
+        }
+      });
+      return filteredEntry;
+    });
+    // console.log(data)
+    return data;
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return (
+        <div className="text-center font-weight-bold">
+          Error: {error.message}
+        </div>
+      );
+    } else if (!isLoaded) {
+      return <div className="text-center font-weight-bold">Loading...</div>;
+    } else {
+      return (
+        <div className="container-fluid m-1">
+          <div className="container bg-light p-2">
+            <a
+              className="btn btn-primary text-left float-left"
+              href="/questionnaire/add"
+            >
+              Physical Suitability Test
+            </a>
+            <br /> <br />
+            <div className="row">
+              <div className="col-12 p-1 m-1">
+                <h2>Donor Physical Suitability Tests</h2>
+                <DataTable
+                  className="table table-stripped table-hover"
+                  columns={this.columns}
+                  data={items}
+                  pagination
+                  pointerOnHover
+                  highlightOnHover
+                  paginationRowsPerPageOptions={[10, 20, 30, 40, 50]}
+                  striped={true}
+                  responsive
+                  noHeader
+                  onRowClicked={(data: any) => {
+                    console.log(data);
+                  }}
+                />
+              </div>
+              <div className="p-2 m-2" aria-readonly></div>
             </div>
-        )
+          </div>
+        </div>
+      );
     }
+  }
 }
 
-export default PhysicalSuitability
+export default PhysicalSuitability;
