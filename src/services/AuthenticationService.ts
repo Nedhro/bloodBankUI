85c5bb08d80c86eb64a1ddf5675506d1b66bb28a
nedhro <nedhro786>
2021-07-10 14:00:53 +0600

    universal cookie added ,Donor Consent Form design update

---
+++ src/services/AuthenticationService.ts
@@ -0,0 +1,14 @@
+import { BehaviorSubject } from 'rxjs';
+
+const currentUserSubject = new BehaviorSubject(localStorage.getItem('currentUser'));
+export const authenticationService = {
+  logout,
+  currentUser: currentUserSubject.asObservable(),
+  get currentUserValue() { return currentUserSubject.value }
+};
+
+function logout() {
+  // remove user from local storage to log user out
+  localStorage.removeItem('currentUser');
+  currentUserSubject.next(null);
+}
